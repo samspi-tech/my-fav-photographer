383ba8ce58585aa1d01dd76cdf84745bdb866313
Andrea Facco <andrew.ryan.r.a@gmail.com>
2025-06-24 19:46:16 +0200

    add models

---
+++ backend/models/photo.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const PhotoSchema = new mongoose.Schema(
+    {
+        photo: {
+            type: String,
+            minLength: 1,
+            maxLength: 255,
+            match: '/^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$/',
+            required: true,
+        },
+        body: {
+            type: String,
+            maxLength: 2550,
+            default: '',
+        },
+        tag: {
+            type: String,
+            maxLength: 255,
+            default: '',
+        },
+    },
+    { timestamps: true, strict: true },
+);
+
+module.exports = mongoose.module('photo', PhotoSchema, 'photos');
