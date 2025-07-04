4e9d9abc6f4ddf855e20c84c14a03cc3906a24cc
Andrea Facco <andrew.ryan.r.a@gmail.com>
2025-07-04 21:49:19 +0200

    add react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage.jsx';
 
 const App = () => {
     return (
-        <>
-            <LoginPage />
-        </>
+        <Router>
+            <Routes>
+                <Route index path="/" element={<LoginPage />} />
+            </Routes>
+        </Router>
     );
 };
 
