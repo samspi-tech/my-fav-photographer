e89e6f9c0fcd5fd8499ff5b65b4779914c27098d
Andrea Facco <andrew.ryan.r.a@gmail.com>
2025-07-05 22:19:55 +0200

    add signup form and validation

---
+++ frontend/src/components/loginCover/LoginCover.jsx
@@ -0,0 +1,7 @@
+const LoginCover = () => {
+    return (
+        <div className="login-cover-container d-flex justify-content-center align-items-center"></div>
+    );
+};
+
+export default LoginCover;
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,19 +1,40 @@
+import { useState } from 'react';
+import { Button } from 'primereact/button';
 import Logo from '../components/logo/Logo.jsx';
 import Login from '../components/login/Login.jsx';
+import Signup from '../components/signup/Signup.jsx';
 import { Col, Container, Row } from 'react-bootstrap';
-import LoginCover from '../components/login/partials/LoginCover.jsx';
+import LoginCover from '../components/loginCover/LoginCover.jsx';
 
 const LoginPage = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const handleIsVisible = () => {
+        setIsVisible((prevState) => !prevState);
+    };
+
     return (
         <Container fluid={true}>
+            <Signup isVisible={isVisible} handleIsVisible={handleIsVisible} />
             <Row className="vh-100 flex-column justify-content-center">
-                <Col lg={4} className="p-0">
+                <Col lg={4}>
                     <Row className="justify-content-center gy-5">
                         <Col xs={12}>
                             <Logo />
                         </Col>
                         <Col xs={12}>
                             <Login />
+                        </Col>
+                        <Col>
+                            <div className="d-flex justify-content-center align-items-center gap-2">
+                                <p className="mb-0 ">Don't have an account?</p>
+                                <Button
+                                    link
+                                    label="Sign up"
+                                    onClick={handleIsVisible}
+                                    className="p-0 text-info shadow-none"
+                                />
+                            </div>
                         </Col>
                     </Row>
                 </Col>
