6e69f12f569821fde6c3b07df1e5f533cb496dd8
Andrea Facco <andrew.ryan.r.a@gmail.com>
2025-07-06 22:39:49 +0200

    add base layout

--- frontend/src/components/navigation/Navigation.jsx
+++ frontend/src/components/navigation/Navigation.jsx
@@ -1,26 +1,28 @@
 import './navigation.css';
 import Logo from '../logo/Logo.jsx';
-import { Col, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import NavEnd from './partials/NavEnd.jsx';
 import { navItems } from './dataSource.jsx';
 import { Menubar } from 'primereact/menubar';
 
 const Navigation = () => {
     return (
-        <nav className="container mt-2">
-            <Row>
-                <Col>
-                    <Menubar
-                        end={<NavEnd />}
-                        model={navItems}
-                        className="d-flex align-items-center"
-                        start={
-                            <Logo cssClass="nav-logo me-5 d-none d-lg-flex" />
-                        }
-                    />
-                </Col>
-            </Row>
-        </nav>
+        <Container fluid={true} className="nav-container">
+            <nav className="container">
+                <Row>
+                    <Col>
+                        <Menubar
+                            end={<NavEnd />}
+                            model={navItems}
+                            className="d-flex align-items-center"
+                            start={
+                                <Logo cssClass="nav-logo me-5 d-none d-lg-flex" />
+                            }
+                        />
+                    </Col>
+                </Row>
+            </nav>
+        </Container>
     );
 };
 
