8c470539bc0a76fc163978fa29eefbe09db09b41
Andrea Facco <andrew.ryan.r.a@gmail.com>
2025-07-10 21:57:29 +0200

    add posts to profile page

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,19 +1,16 @@
 import React, { useContext, useEffect } from 'react';
 import BaseLayout from '../baseLayout/BaseLayout.jsx';
+import Profile from '../components/profile/Profile.jsx';
 import { UserContext } from '../contexts/UserContext.jsx';
 
 const ProfilePage = () => {
-    const { getMe } = useContext(UserContext);
+    const { getMe, user } = useContext(UserContext);
 
     useEffect(() => {
         getMe();
     }, []);
 
-    return (
-        <BaseLayout>
-            <p>profile page</p>
-        </BaseLayout>
-    );
+    return <BaseLayout>{user && <Profile user={user} />}</BaseLayout>;
 };
 
 export default ProfilePage;
