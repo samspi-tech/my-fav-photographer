2743e8232ac825dd96423881106f0b4ad869a2f4
Andrea Facco <andrew.ryan.r.a@gmail.com>
2025-07-14 22:59:20 +0200

    added address context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,28 +10,31 @@ import { UserProvider } from './contexts/UserContext.jsx';
 import { PostProvider } from './contexts/PostContext.jsx';
 import { PhotoProvider } from './contexts/PhotoContext.jsx';
 import { CommentProvider } from './contexts/CommentContext.jsx';
+import { AddressProvider } from './contexts/AddressContext.jsx';
 import { WorkshopProvider } from './contexts/WorkshopContext.jsx';
-import { EquipmentProvider } from './contexts/EquipmentContext.jsx';
 import { FollowerProvider } from './contexts/FollowerContext.jsx';
+import { EquipmentProvider } from './contexts/EquipmentContext.jsx';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <UserProvider>
-            <FollowerProvider>
-                <PostProvider>
-                    <CommentProvider>
-                        <PhotoProvider>
-                            <EquipmentProvider>
-                                <WorkshopProvider>
-                                    <PrimeReactProvider>
-                                        <App />
-                                    </PrimeReactProvider>
-                                </WorkshopProvider>
-                            </EquipmentProvider>
-                        </PhotoProvider>
-                    </CommentProvider>
-                </PostProvider>
-            </FollowerProvider>
+            <AddressProvider>
+                <FollowerProvider>
+                    <PostProvider>
+                        <CommentProvider>
+                            <PhotoProvider>
+                                <EquipmentProvider>
+                                    <WorkshopProvider>
+                                        <PrimeReactProvider>
+                                            <App />
+                                        </PrimeReactProvider>
+                                    </WorkshopProvider>
+                                </EquipmentProvider>
+                            </PhotoProvider>
+                        </CommentProvider>
+                    </PostProvider>
+                </FollowerProvider>
+            </AddressProvider>
         </UserProvider>
     </StrictMode>,
 );
