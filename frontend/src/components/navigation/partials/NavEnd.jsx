004adda4930940f3107de42fceb161771600f917
Andrea Facco <andrew.ryan.r.a@gmail.com>
2025-07-23 21:48:54 +0200

    refactor: navbar

--- frontend/src/components/navigation/partials/NavEnd.jsx
+++ frontend/src/components/navigation/partials/NavEnd.jsx
@@ -1,31 +1,93 @@
-import { useContext } from 'react';
+import { Menu } from 'primereact/menu';
 import About from './about/About.jsx';
 import { Avatar } from 'primereact/avatar';
-import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
 import Searchbar from './searchbar/Searchbar.jsx';
+import { useContext, useRef, useState } from 'react';
 import { useLogin } from '../../../hooks/useLogin.js';
+import FollowList from '../../followList/FollowList.jsx';
 import ShoppingCart from '../../shoppingCart/ShoppingCart.jsx';
 import { UserContext } from '../../../contexts/UserContext.jsx';
 
 const NavEnd = ({ user, isRoleUser }) => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const handleVisibility = () => {
+        setIsVisible((prevState) => !prevState);
+    };
+
+    const configMenu = useRef(null);
     const { setUser } = useContext(UserContext);
     const { logout } = useLogin(null, setUser);
 
+    const items = [
+        {
+            label: 'Settings',
+            icon: 'pi pi-cog',
+            url: '/settings',
+        },
+        {
+            label: 'Logout',
+            icon: 'pi pi-sign-out',
+            command: logout,
+        },
+    ];
+
+    const userItems = [
+        {
+            label: 'Following',
+            icon: 'pi pi-camera',
+            command: handleVisibility,
+        },
+        ...items,
+    ];
+
+    const photographerItems = [
+        {
+            label: 'Profile',
+            icon: 'pi pi-user',
+            url: '/profile',
+        },
+        ...items,
+    ];
+
     return (
-        <div className="d-flex align-items-center">
-            {isRoleUser && <Searchbar />}
-            {isRoleUser && <ShoppingCart />}
-            {user && <Avatar size="large" shape="circle" image={user.avatar} />}
-            <About />
-            <Button
-                text
-                onClick={logout}
-                tooltip="Logout"
-                icon="pi pi-sign-out"
-                className="nav-end-icon shadow-none bg-transparent"
-                tooltipOptions={{ position: 'bottom' }}
-            />
-        </div>
+        <>
+            <div className="d-flex align-items-center">
+                {isRoleUser && <Searchbar />}
+                {isRoleUser && <ShoppingCart />}
+                <div>
+                    <Menu
+                        popup
+                        ref={configMenu}
+                        id="config_menu"
+                        className="custom-menu"
+                        model={isRoleUser ? userItems : photographerItems}
+                    />
+                    <button
+                        className="p-panel-header-icon p-link mx-2"
+                        onClick={(e) => configMenu?.current?.toggle(e)}
+                    >
+                        {user && (
+                            <Avatar
+                                size="large"
+                                shape="circle"
+                                image={user.avatar}
+                            />
+                        )}
+                    </button>
+                </div>
+                <About />
+            </div>
+            <Dialog
+                visible={isVisible}
+                onHide={handleVisibility}
+                className="custom-dialog"
+                header="Photographers list"
+            >
+                <FollowList />
+            </Dialog>
+        </>
     );
 };
 
