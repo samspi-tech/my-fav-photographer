fb118105be441eb5dc5d21e686fbc902b4bdf3e9
samspi-tech <andrew.ryan.r.a@gmail.com>
2025-11-29 00:35:15 +0100

    add icons to nav links

--- frontend/src/components/mainNav/MainNav.jsx
+++ frontend/src/components/mainNav/MainNav.jsx
@@ -2,6 +2,7 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import styles from './MainNav.module.css';
 import Logo from '../logo/Logo';
+import { HiHome, HiUser, HiUsers } from 'react-icons/hi2';
 
 const MainNav = () => {
     return (
@@ -11,9 +12,18 @@ const MainNav = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className={`me-auto ${styles.navLinksContainer}`}>
-                        <NavLink to="/">Home</NavLink>
-                        <NavLink to="/photographers">Photographers</NavLink>
-                        <NavLink to="profile">Profile</NavLink>
+                        <NavLink to="/">
+                            <HiHome />
+                            <span>Home</span>
+                        </NavLink>
+                        <NavLink to="/photographers">
+                            <HiUsers />
+                            <span>Photographers</span>
+                        </NavLink>
+                        <NavLink to="profile">
+                            <HiUser />
+                            <span>Profile</span>
+                        </NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
