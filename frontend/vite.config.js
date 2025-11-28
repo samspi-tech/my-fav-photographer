66fce851e2e7a6b28ba9edfed0454519b32e0d00
samspi-tech <andrew.ryan.r.a@gmail.com>
2025-11-28 21:15:45 +0100

    add eslintrc.json file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import eslint from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), eslint()],
+    resolve: {
+        alias: {
+            // eslint-disable-next-line no-undef
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
 });
