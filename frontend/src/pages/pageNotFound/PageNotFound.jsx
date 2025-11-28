2e3fe9ece21618fcd88028f7024817be07f8bd69
samspi-tech <andrew.ryan.r.a@gmail.com>
2025-11-28 20:42:58 +0100

    add react icons and updated index.css

--- frontend/src/pages/pageNotFound/PageNotFound.jsx
+++ frontend/src/pages/pageNotFound/PageNotFound.jsx
@@ -1,12 +1,22 @@
-import { Container } from 'react-bootstrap';
+import { HiArrowLeft } from 'react-icons/hi2';
+import { Link } from 'react-router-dom';
+import { Col, Container, Row } from 'react-bootstrap';
 import styles from './PageNotFound.module.css';
 
 const PageNotFound = () => {
     return (
         <Container className={styles.container}>
-            <h3>
-                404 <span>|</span> Page not found
-            </h3>
+            <Row>
+                <Col>
+                    <h2>
+                        404 <span>|</span> Page Not Found
+                    </h2>
+                    <Link to="/" replace>
+                        <HiArrowLeft />
+                        <span>Back</span>
+                    </Link>
+                </Col>
+            </Row>
         </Container>
     );
 };
