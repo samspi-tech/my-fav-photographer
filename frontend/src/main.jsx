6764ec85c6d735cb17c9da1767abc077c00b3f7f
samspi-tech <andrew.ryan.r.a@gmail.com>
2025-11-28 12:34:04 +0100

    removed contexts from main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,43 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'primeicons/primeicons.css';
-import 'primereact/resources/themes/tailwind-light/theme.css';
 import './index.css';
 import App from './App.jsx';
-import { PrimeReactProvider } from 'primereact/api';
-import { UserProvider } from './contexts/UserContext.jsx';
-import { PostProvider } from './contexts/PostContext.jsx';
-import { PhotoProvider } from './contexts/PhotoContext.jsx';
-import { CommentProvider } from './contexts/CommentContext.jsx';
-import { AddressProvider } from './contexts/AddressContext.jsx';
-import { WorkshopProvider } from './contexts/WorkshopContext.jsx';
-import { FollowerProvider } from './contexts/FollowerContext.jsx';
-import { EquipmentProvider } from './contexts/EquipmentContext.jsx';
-import { ShoppingCartProvider } from './contexts/ShoppingCartContext.jsx';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <UserProvider>
-            <ShoppingCartProvider>
-                <AddressProvider>
-                    <FollowerProvider>
-                        <PostProvider>
-                            <CommentProvider>
-                                <PhotoProvider>
-                                    <EquipmentProvider>
-                                        <WorkshopProvider>
-                                            <PrimeReactProvider>
-                                                <App />
-                                            </PrimeReactProvider>
-                                        </WorkshopProvider>
-                                    </EquipmentProvider>
-                                </PhotoProvider>
-                            </CommentProvider>
-                        </PostProvider>
-                    </FollowerProvider>
-                </AddressProvider>
-            </ShoppingCartProvider>
-        </UserProvider>
+        <App />
     </StrictMode>
 );
