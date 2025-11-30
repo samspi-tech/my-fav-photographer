a0fd372d74a4053e35931a43e8d85d3d1c9112a7
samspi-tech <andrew.ryan.r.a@gmail.com>
2025-11-30 19:24:42 +0100

    refactor(useScrollToTop): increased setTimeout to 3s

--- frontend/src/hooks/useScrollToTop.js
+++ frontend/src/hooks/useScrollToTop.js
@@ -23,7 +23,7 @@ export const useScrollToTop = () => {
 
         const hideButton = setTimeout(() => {
             setIsVisible(false);
-        }, 2000);
+        }, 3000);
 
         return () => {
             clearTimeout(hideButton);
